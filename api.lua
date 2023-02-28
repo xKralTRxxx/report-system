059a0011786e26ce8332dac4549821f93ab1625e
xKralTRxxx <99412992+xKralTRxxx@users.noreply.github.com>
2023-02-28 17:57:40 +0300

    Update api.lua

--- api.lua
+++ api.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Player = game.Players.LocalPlayer
 local plrname = Player.name
 local HS = game:GetService("HttpService")
@@ -6,7 +6,7 @@ local WebhookURL = "https://discord.com/api/webhooks/1080135401529954386/7oAqpI_
 local message = "???"
 
 local MsgPrivate = {
-	["content"] = "nickname: "..Player.name.." reported: "message""
+	["content"] = "nickname: " ..Player.name.. " reported: "message""
 }
 
 MsgPrivate = HS:JSONEncode(MsgPrivate)
